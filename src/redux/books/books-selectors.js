94043c7679a365b34e501952e18cb2465545193e
RomaSamkov <Romansamkov.rs@gmail.com>
2023-02-07 20:20:09 +0200

    Create books-selectors.js

---
+++ src/redux/books/books-selectors.js
@@ -0,0 +1,18 @@
+export const getBooks = (store) => store.books;
+export const getFavoriteBooks = (store) => {
+  const favoriteBooks = store.books.filter(({ favorite }) => favorite);
+  return favoriteBooks;
+};
+
+export const getFilteredBooks = ({ books, filter }) => {
+  const normalizedFilter = filter.toLowerCase();
+  const result = books.filter(({ title, author }) => {
+    const normalizedTitle = title.toLowerCase();
+    const normalizedAuthor = author.toLowerCase();
+    return (
+      normalizedTitle.includes(normalizedFilter) ||
+      normalizedAuthor.includes(normalizedFilter)
+    );
+  });
+  return result;
+};
