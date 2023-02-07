cd1dafcde4ed07b51b7204d51fe53e4fb699d392
RomaSamkov <Romansamkov.rs@gmail.com>
2023-02-07 20:20:29 +0200

    Create rootReducer.js

---
+++ src/redux/rootReducer.js
@@ -0,0 +1,10 @@
+import { combineReducers } from "redux";
+import booksReducer from "./books/books-reducer";
+import filterReducer from "./filter/filter-reducer";
+
+const rootReducer = combineReducers({
+  books: booksReducer,
+  filter: filterReducer,
+});
+
+export default rootReducer;
