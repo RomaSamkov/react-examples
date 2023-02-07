84359f20f696eaf9f4712ce10c6247c4bd8178b7
RomaSamkov <Romansamkov.rs@gmail.com>
2023-02-07 20:19:51 +0200

    Update HomePage.jsx

--- src/pages/HomePage/HomePage.jsx
+++ src/pages/HomePage/HomePage.jsx
@@ -1,11 +1,9 @@
-import Posts from "modules/Posts/Posts";
 import React from "react";
 
 const HomePage = () => {
   return (
     <div className="container">
       <h2>Home Page</h2>
-      <Posts />
     </div>
   );
 };
