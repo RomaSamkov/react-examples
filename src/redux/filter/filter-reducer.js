9accea81dac47830c0550d053da8dcd6ef36da5d
RomaSamkov <Romansamkov.rs@gmail.com>
2023-02-08 20:48:34 +0200

    add createReducer

--- src/redux/filter/filter-reducer.js
+++ src/redux/filter/filter-reducer.js
@@ -1,15 +1,8 @@
-import { SET_FILTER } from "./filter-types";
+import { createReducer } from "@reduxjs/toolkit";
+import { setFilter } from "./filter-actions";
 
-const initialStore = "";
-
-const filterReducer = (store = initialStore, { type, payload }) => {
-  switch (type) {
-    case SET_FILTER:
-      return payload;
-
-    default:
-      return store;
-  }
-};
+const filterReducer = createReducer("", {
+  [setFilter]: (_, { payload }) => payload,
+});
 
 export default filterReducer;
