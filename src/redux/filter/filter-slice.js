0bfba8d5be32dc683777da56e8541982ad2afb71
RomaSamkov <Romansamkov.rs@gmail.com>
2023-02-09 15:55:33 +0200

    Create filter-slice.js

---
+++ src/redux/filter/filter-slice.js
@@ -0,0 +1,12 @@
+const { createSlice } = require("@reduxjs/toolkit");
+
+const filterSlice = createSlice({
+  name: "filter",
+  initialState: "",
+  reducers: {
+    setFilter: (_, { payload }) => payload,
+  },
+});
+
+export const { setFilter } = filterSlice.actions;
+export default filterSlice.reducer;
