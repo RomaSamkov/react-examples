f847ba1662ffc7b219c5fe6836e4ef1d52bca812
RomaSamkov <Romansamkov.rs@gmail.com>
2023-02-09 21:32:12 +0200

    add PersistGate

--- src/index.js
+++ src/index.js
@@ -1,7 +1,8 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
+import { PersistGate } from "redux-persist/integration/react";
 import { BrowserRouter } from "react-router-dom";
-import store from "redux/store";
+import { store, persistor } from "redux/store";
 import { Provider } from "react-redux";
 
 import App from "./App";
@@ -10,9 +11,11 @@ const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
     <Provider store={store}>
-      <BrowserRouter>
-        <App />
-      </BrowserRouter>
+      <PersistGate loading={null} persistor={persistor}>
+        <BrowserRouter>
+          <App />
+        </BrowserRouter>
+      </PersistGate>
     </Provider>
   </React.StrictMode>
 );
