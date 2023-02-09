69341d4ad30fa0fb7f58f9249636205291ac8542
RomaSamkov <Romansamkov.rs@gmail.com>
2023-02-09 15:55:24 +0200

    add slice

--- src/redux/store.js
+++ src/redux/store.js
@@ -1,6 +1,6 @@
 import { configureStore } from "@reduxjs/toolkit";
-import booksReducer from "./books/books-reducer";
-import filterReducer from "./filter/filter-reducer";
+import booksReducer from "./books/books-slice";
+import filterReducer from "./filter/filter-slice";
 
 const store = configureStore({
   reducer: {
