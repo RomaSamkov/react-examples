fcb7a5ae6162400894d0d47c991e41b541ff504c
RomaSamkov <Romansamkov.rs@gmail.com>
2023-02-09 21:33:06 +0200

    add persistStore

--- src/redux/store.js
+++ src/redux/store.js
@@ -1,12 +1,24 @@
-import { configureStore } from "@reduxjs/toolkit";
+import { combineReducers, configureStore } from "@reduxjs/toolkit";
+import { persistStore, persistReducer } from "redux-persist";
+import storage from "redux-persist/lib/storage";
 import booksReducer from "./books/books-slice";
 import filterReducer from "./filter/filter-slice";
 
-const store = configureStore({
-  reducer: {
-    books: booksReducer,
-    filter: filterReducer,
-  },
+const rootReducer = combineReducers({
+  books: booksReducer,
+  filter: filterReducer,
 });
 
-export default store;
+const persistConfig = {
+  key: "books",
+  storage,
+  whitelist: ["books"],
+};
+
+const persistedReducer = persistReducer(persistConfig, rootReducer);
+
+export const store = configureStore({
+  reducer: persistedReducer,
+});
+
+export const persistor = persistStore(store);
