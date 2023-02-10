550aaea204b8d3c8587b931ae8a8c11e2317ae89
RomaSamkov <Romansamkov.rs@gmail.com>
2023-02-10 21:08:17 +0200

    Update books-selectors.js

--- src/redux/books/books-selectors.js
+++ src/redux/books/books-selectors.js
@@ -1,18 +1,21 @@
-export const getBooks = (store) => store.books;
-export const getFavoriteBooks = (store) => {
-  const favoriteBooks = store.books.filter(({ favorite }) => favorite);
-  return favoriteBooks;
+export const getBooks = store => store.books;
+
+export const getFilteredBooks = ({books, filter}) => {
+    if(!filter) {
+        return books;
+    }
+    const normalizedFilter = filter.toLowerCase();
+    const result = books.filter(({title, author}) => {
+        const normalizedTitle = title.toLowerCase();
+        const normalizedAuthor = author.toLowerCase();
+        return (normalizedTitle.includes(normalizedFilter) || normalizedAuthor.includes(normalizedFilter))
+    });
+
+    return result;
+}
+
+export const getFavoriteBooks = ({books}) => {
+    const favoriteBooks = books.filter(({favorite}) => favorite);
+    return favoriteBooks;
 };
 
-export const getFilteredBooks = ({ books, filter }) => {
-  const normalizedFilter = filter.toLowerCase();
-  const result = books.filter(({ title, author }) => {
-    const normalizedTitle = title.toLowerCase();
-    const normalizedAuthor = author.toLowerCase();
-    return (
-      normalizedTitle.includes(normalizedFilter) ||
-      normalizedAuthor.includes(normalizedFilter)
-    );
-  });
-  return result;
-};
