89eb9feb5697d614d1d4212afedfef14e5ba33b4
RomaSamkov <Romansamkov.rs@gmail.com>
2023-02-11 21:14:01 +0200

    Update books-actions.js

--- src/redux/books/books-actions.js
+++ src/redux/books/books-actions.js
@@ -1,13 +1,17 @@
-import {createAction} from "@reduxjs/toolkit";
+import { createAction } from "@reduxjs/toolkit";
 import { nanoid } from "nanoid";
 
-export const addBook = createAction("books/add", (data)=> {
-    return {
-        payload: {
-            ...data,
-            id: nanoid()
-        }
-    }
+export const fetchBooksLoading = createAction("books/fetch/loading");
+export const fetchBooksSuccess = createAction("books/fetch/success");
+export const fetchBooksError = createAction("books/fetch/error");
+
+export const addBook = createAction("books/add", (data) => {
+  return {
+    payload: {
+      ...data,
+      id: nanoid(),
+    },
+  };
 });
 
 export const removeBook = createAction("books/remove");
