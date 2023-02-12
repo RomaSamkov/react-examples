409a1f86c2086ab7c859840bde1b4449cbc5e0a5
RomaSamkov <Romansamkov.rs@gmail.com>
2023-02-12 21:29:39 +0200

    Update books-operations.js

--- src/redux/books/books-operations.js
+++ src/redux/books/books-operations.js
@@ -1,19 +1,33 @@
-import { getBooks } from "shared/api/books";
-import {
-  fetchBooksError,
-  fetchBooksLoading,
-  fetchBooksSuccess,
-} from "./books-actions";
+import * as api from "shared/api/books";
+import * as actions from "./books-actions";
 
 export const fetchBooks = () => {
   const func = async (dispatch) => {
     try {
-      dispatch(fetchBooksLoading());
-      const data = await getBooks();
-      dispatch(fetchBooksSuccess(data));
+      dispatch(actions.fetchBooksLoading());
+      const data = await api.getBooks();
+      dispatch(actions.fetchBooksSuccess(data));
     } catch (error) {
-      dispatch(fetchBooksError(error));
+      dispatch(actions.fetchBooksError(error));
     }
   };
+  return func;
+};
+
+export const addBook = (data) => {
+  const func = async (dispatch) => {
+    try {
+      dispatch(actions.addBookLoading());
+      const result = await api.addBook(data);
+      dispatch(actions.addBookSuccess(result));
+    } catch (error) {
+      dispatch(actions.addBookError(error.message));
+    }
+  };
+  return func;
+};
+
+export const removeBook = (id) => {
+  const func = (dispatch) => {};
   return func;
 };
