8d6b937c65ab05f287d8611e5915a1b9da55ad70
RomaSamkov <Romansamkov.rs@gmail.com>
2023-02-14 15:49:51 +0200

    add createAsyncThunk

--- src/redux/books/books-operations.js
+++ src/redux/books/books-operations.js
@@ -1,18 +1,30 @@
+import { createAsyncThunk } from "@reduxjs/toolkit";
 import * as api from "shared/api/books";
-import * as actions from "./books-actions";
 
-export const fetchBooks = () => {
-  const func = async (dispatch) => {
+export const fetchBooks = createAsyncThunk(
+  "books/fetch",
+  async (_, thunkAPI) => {
     try {
-      dispatch(actions.fetchBooksLoading());
       const data = await api.getBooks();
-      dispatch(actions.fetchBooksSuccess(data));
+      return data;
     } catch (error) {
-      dispatch(actions.fetchBooksError(error));
+      return thunkAPI.rejectWithValue(error);
     }
-  };
-  return func;
-};
+  }
+);
+
+// export const fetchBooks = () => {
+//   const func = async (dispatch) => {
+//     try {
+//       dispatch(actions.fetchBooksLoading());
+//       const data = await api.getBooks();
+//       dispatch(actions.fetchBooksSuccess(data));
+//     } catch (error) {
+//       dispatch(actions.fetchBooksError(error));
+//     }
+//   };
+//   return func;
+// };
 
 const isDublicate = ({ title, author }, books) => {
   const normalizedTitle = title.toLowerCase();
@@ -26,33 +38,64 @@ const isDublicate = ({ title, author }, books) => {
   return Boolean(result);
 };
 
-export const addBook = (data) => {
-  const func = async (dispatch, getState) => {
-    const { books } = getState();
-    if (isDublicate(data, books.items)) {
-      return alert(`${data.title} or ${data.author} is already exist !`);
-    }
-
+export const addBook = createAsyncThunk(
+  "books/add",
+  async (data, { rejectWithValue }) => {
     try {
-      dispatch(actions.addBookLoading());
       const result = await api.addBook(data);
-      dispatch(actions.addBookSuccess(result));
+      return result;
     } catch (error) {
-      dispatch(actions.addBookError(error.message));
+      return rejectWithValue(error);
     }
-  };
-  return func;
-};
-
-export const removeBook = (id) => {
-  const func = async (dispatch) => {
+  },
+  {
+    condition: (data, { getState }) => {
+      const { books } = getState();
+      if (isDublicate(data, books.items)) {
+        alert(`${data.title} ${data.author} is already exist !`);
+        return false;
+      }
+    },
+  }
+);
+export const removeBook = createAsyncThunk(
+  "books/remove",
+  async (id, { rejectWithValue }) => {
     try {
-      dispatch(actions.removeBookLoading());
       await api.removeBook(id);
-      dispatch(actions.removeBookSuccess(id));
+      return id;
     } catch (error) {
-      dispatch(actions.removeBookError(error.message));
+      return rejectWithValue(error);
     }
-  };
-  return func;
-};
+  }
+);
+// export const addBook = (data) => {
+//   const func = async (dispatch, getState) => {
+//     const { books } = getState();
+//     if (isDublicate(data, books.items)) {
+//       return alert(`${data.title} or ${data.author} is already exist !`);
+//     }
+
+//     try {
+//       dispatch(actions.addBookLoading());
+//       const result = await api.addBook(data);
+//       dispatch(actions.addBookSuccess(result));
+//     } catch (error) {
+//       dispatch(actions.addBookError(error.message));
+//     }
+//   };
+//   return func;
+// };
+
+// export const removeBook = (id) => {
+//   const func = async (dispatch) => {
+//     try {
+//       dispatch(actions.removeBookLoading());
+//       await api.removeBook(id);
+//       dispatch(actions.removeBookSuccess(id));
+//     } catch (error) {
+//       dispatch(actions.removeBookError(error.message));
+//     }
+//   };
+//   return func;
+// };
