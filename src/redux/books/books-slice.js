303f844cef5a6ba61cf1e5569e2852de46d506cc
RomaSamkov <Romansamkov.rs@gmail.com>
2023-02-14 15:50:06 +0200

    Create books-slice.js

---
+++ src/redux/books/books-slice.js
@@ -0,0 +1,53 @@
+const { createSlice } = require("@reduxjs/toolkit");
+const { fetchBooks, addBook, removeBook } = require("./books-operations");
+
+const initialState = {
+  items: [],
+  loading: false,
+  error: null,
+};
+
+const booksSlice = createSlice({
+  name: "books",
+  initialState,
+  extraReducers: {
+    [fetchBooks.pending]: (store) => {
+      store.loading = true;
+      store.error = null;
+    },
+    [fetchBooks.fulfilled]: (store, { payload }) => {
+      store.loading = false;
+      store.items = payload;
+    },
+    [fetchBooks.rejected]: (store, { payload }) => {
+      store.loading = false;
+      store.error = payload;
+    },
+    [addBook.pending]: (store) => {
+      store.loading = true;
+      store.error = null;
+    },
+    [addBook.fulfilled]: (store, { payload }) => {
+      store.loading = false;
+      store.items.push(payload);
+    },
+    [addBook.rejected]: (store, { payload }) => {
+      store.loading = false;
+      store.error = payload;
+    },
+    [removeBook.pending]: (store) => {
+      store.loading = true;
+      store.error = null;
+    },
+    [removeBook.fulfilled]: (store, { payload }) => {
+      store.loading = false;
+      store.items = store.items.filter((item) => item.id !== payload);
+    },
+    [removeBook.rejected]: (store, { payload }) => {
+      store.loading = false;
+      store.error = payload;
+    },
+  },
+});
+
+export default booksSlice.reducer;
