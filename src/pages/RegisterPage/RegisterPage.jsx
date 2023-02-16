1a6e8181e510b1b1b23c0668baf18e630ad16344
RomaSamkov <Romansamkov.rs@gmail.com>
2023-02-16 21:23:50 +0200

    Create RegisterPage.jsx

---
+++ src/pages/RegisterPage/RegisterPage.jsx
@@ -0,0 +1,13 @@
+import RegisterForm from "modules/RegisterForm";
+import React from "react";
+
+const RegisterPage = () => {
+  return (
+    <div className="container">
+      <h2>Register Page</h2>
+      <RegisterForm />
+    </div>
+  );
+};
+
+export default RegisterPage;
