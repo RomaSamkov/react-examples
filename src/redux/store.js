7e1b7d7092653f64f7353ddb2e062ed51cfcb559
RomaSamkov <Romansamkov.rs@gmail.com>
2023-02-17 21:34:09 +0200

    Update store.js

--- src/redux/store.js
+++ src/redux/store.js
@@ -1,10 +1,12 @@
 import { configureStore } from "@reduxjs/toolkit";
 
+import authReducer from "./auth/auth-slice";
 import booksReducer from "./books/books-slice";
 import filterReducer from "./filter/filter-reducer";
 
 const store = configureStore({
   reducer: {
+    auth: authReducer,
     books: booksReducer,
     filter: filterReducer,
   },
