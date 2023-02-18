a41dcce35d6e97e6bee0c2f031067e9e4322cee2
RomaSamkov <Romansamkov.rs@gmail.com>
2023-02-18 20:45:04 +0200

    add onRegister

--- src/pages/RegisterPage/RegisterPage.jsx
+++ src/pages/RegisterPage/RegisterPage.jsx
@@ -1,11 +1,27 @@
 import RegisterForm from "modules/RegisterForm";
 import React from "react";
+import { useDispatch, useSelector } from "react-redux";
+import { Navigate } from "react-router-dom";
+import { signup } from "redux/auth/auth-operations";
+import { getAuthError, isAuth } from "redux/auth/auth-selectors";
 
 const RegisterPage = () => {
+  const dispatch = useDispatch();
+  const { status, statusText } = useSelector(getAuthError);
+  const isLogin = useSelector(isAuth);
+
+  const onRegister = (data) => {
+    dispatch(signup(data));
+  };
+
+  if (isLogin) {
+    return <Navigate to="/books" />;
+  }
   return (
     <div className="container">
       <h2>Register Page</h2>
-      <RegisterForm />
+      <RegisterForm onSubmit={onRegister} />
+      {status && <p style={{ color: "red" }}>{statusText}</p>}
     </div>
   );
 };
