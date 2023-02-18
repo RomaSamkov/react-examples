9aa125142aa0aedb91e3af727155e579161ff130
RomaSamkov <Romansamkov.rs@gmail.com>
2023-02-18 20:45:56 +0200

    add LoginPage

--- src/UserRoutes.jsx
+++ src/UserRoutes.jsx
@@ -5,6 +5,7 @@ const MyFavoriteBooksPage = lazy(() =>
   import("pages/MyFavoriteBooksPage/MyFavoriteBooksPage")
 );
 const RegisterPage = lazy(() => import("pages/RegisterPage/RegisterPage"));
+const LoginPage = lazy(() => import("pages/LoginPage/LoginPage"));
 const BooksPage = lazy(() => import("pages/BooksPage/BooksPage"));
 const HomePage = lazy(() => import("pages/HomePage/HomePage"));
 const NotFoundPage = lazy(() => import("pages/NotFoundPage/NotFoundPage"));
@@ -15,6 +16,7 @@ const UserRoutes = () => {
       <Routes>
         <Route path="/" element={<HomePage />} />
         <Route path="/register" element={<RegisterPage />} />
+        <Route path="/login" element={<LoginPage />} />
         <Route path="/books" element={<BooksPage />} />
         <Route path="/my-favorite-books" element={<MyFavoriteBooksPage />} />
         <Route path="*" element={<NotFoundPage />} />
