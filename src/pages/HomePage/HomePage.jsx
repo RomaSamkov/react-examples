a23602f5614c955721ab6e31be0078efcf657907
RomaSamkov <Romansamkov.rs@gmail.com>
2023-02-19 21:54:15 +0200

    Update HomePage.jsx

--- src/pages/HomePage/HomePage.jsx
+++ src/pages/HomePage/HomePage.jsx
@@ -1,9 +1,20 @@
 import React from "react";
+import { NavLink } from "react-router-dom";
+import useAuth from "shared/hooks/useAuth";
+import styles from "./HomePage.module.scss";
 
 const HomePage = () => {
+  const isLogin = useAuth();
   return (
     <div className="container">
       <h2>Home Page</h2>
+      {!isLogin && (
+        <NavLink to="/books" className={styles.link}>
+          If you see this text it means that you are not authorized! <br />
+          If you want to see Books ----
+          <span className={styles.click}> just click here</span>
+        </NavLink>
+      )}
     </div>
   );
 };
