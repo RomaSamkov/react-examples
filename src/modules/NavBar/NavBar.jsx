1587b958d9e1ac9f1097c0f8a38e5c32ca6b9e6d
RomaSamkov <Romansamkov.rs@gmail.com>
2023-02-19 21:54:04 +0200

    Update NavBar.jsx

--- src/modules/NavBar/NavBar.jsx
+++ src/modules/NavBar/NavBar.jsx
@@ -1,17 +1,20 @@
 import { Link } from "react-router-dom";
+import useAuth from "shared/hooks/useAuth";
 
 import styles from "./NavBar.module.scss";
 import NavBarAuth from "./NavBarAuth";
 import NavBarMenu from "./NavBarMenu";
+import NavBarUser from "./NavBarUser/NavBarUser";
 
 const NavBar = () => {
+  const isLogin = useAuth();
   return (
     <div className={styles.wrapper}>
       <Link to="/" className={styles.logo}>
         L o g o
       </Link>
-      <NavBarMenu />
-      <NavBarAuth />
+      {isLogin && <NavBarMenu />}
+      {isLogin ? <NavBarUser /> : <NavBarAuth />}
     </div>
   );
 };
