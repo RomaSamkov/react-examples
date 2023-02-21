209fc37eb40adfa8b33390d0257bbfebb095573f
RomaSamkov <Romansamkov.rs@gmail.com>
2023-02-21 21:01:07 +0200

    add logout and getCurrent

--- src/redux/auth/auth-slice.js
+++ src/redux/auth/auth-slice.js
@@ -1,5 +1,5 @@
 import { createSlice } from "@reduxjs/toolkit";
-import { login, signup } from "./auth-operations";
+import { getCurrent, login, logout, signup } from "./auth-operations";
 
 const initialState = {
   user: {},
@@ -40,6 +40,34 @@ const authSlice = createSlice({
     [login.rejected]: (store, { payload }) => {
       store.loading = false;
       store.error = payload;
+    },
+    [logout.pending]: (store) => {
+      store.loading = true;
+      store.error = null;
+    },
+    [logout.fulfilled]: (store) => {
+      store.user = {};
+      store.isLogin = false;
+      store.token = "";
+      store.loading = false;
+    },
+    [logout.rejected]: (store, { payload }) => {
+      store.loading = false;
+      store.error = payload;
+    },
+    [getCurrent.pending]: (store) => {
+      store.loading = true;
+      store.error = null;
+    },
+    [getCurrent.fulfilled]: (store, { payload }) => {
+      store.user = payload;
+      store.isLogin = true;
+      store.loading = false;
+    },
+    [getCurrent.rejected]: (store, { payload }) => {
+      store.loading = false;
+      store.error = payload;
+      store.token = "";
     },
   },
 });
