ec06f5c35fb99188b3e5f9276cb3df87a8a106d3
RomaSamkov <Romansamkov.rs@gmail.com>
2023-02-21 20:59:13 +0200

    add useEffect

--- src/App.js
+++ src/App.js
@@ -1,8 +1,16 @@
 import "./App.css";
 import NavBar from "modules/NavBar";
 import UserRoutes from "UserRoutes";
+import { useDispatch } from "react-redux";
+import { useEffect } from "react";
+import { getCurrent } from "redux/auth/auth-operations";
 
 function App() {
+  const dispatch = useDispatch();
+  useEffect(() => {
+    dispatch(getCurrent());
+  }, [dispatch]);
+
   return (
     <div className="App">
       <NavBar />
