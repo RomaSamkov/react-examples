b26430f26433626e6d1cdc8c001ce86becafac5e
RomaSamkov <Romansamkov.rs@gmail.com>
2023-02-21 21:00:04 +0200

    add onLogout

--- src/modules/NavBar/NavBarUser/NavBarUser.jsx
+++ src/modules/NavBar/NavBarUser/NavBarUser.jsx
@@ -1,11 +1,19 @@
-import { useSelector } from "react-redux";
+import { useDispatch, useSelector } from "react-redux";
+import { logout } from "redux/auth/auth-operations";
 import { getUser } from "redux/auth/auth-selectors";
 
 const NavBarUser = () => {
   const { name } = useSelector(getUser);
+  const dispatch = useDispatch();
+
+  const onLogout = () => dispatch(logout());
+
   return (
     <div>
-      <span>{name}</span> | <button type="button">Log Out</button>
+      <span>{name}</span> |{" "}
+      <button type="button" onClick={onLogout}>
+        Log Out
+      </button>
     </div>
   );
 };
