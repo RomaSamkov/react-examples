989b5271622df7beab756a362929ec790bdd5881
RomaSamkov <Romansamkov.rs@gmail.com>
2023-02-27 21:36:40 +0200

    Update HomePage.jsx

--- src/pages/HomePage/HomePage.jsx
+++ src/pages/HomePage/HomePage.jsx
@@ -6,13 +6,35 @@ import styles from "./HomePage.module.scss";
 const HomePage = () => {
   const isLogin = useAuth();
   return (
-    <div className="container">
-      <h2>Home Page</h2>
-      {!isLogin && (
+    <div className={styles.container}>
+      <h2 className={styles.title}>Home Page</h2>
+      {!isLogin ? (
+        <div className={styles.wrapper}>
+          <h2>This is my student draft while studying : </h2>
+          <ul>
+            <li className={styles.fontFace}>Redux Toolkit</li>
+            <li className={styles.fontFaceText}>Login</li>
+            <li className={styles.fontFaceText}>Authrization</li>
+          </ul>
+          <div className={styles.auth}>
+            <NavLink to="/register" className={styles.text}>
+              Register
+            </NavLink>
+            |
+            <NavLink to="login" className={styles.text}>
+              Login
+            </NavLink>
+          </div>
+          <p>
+            If you want to see a book-adding app just click on
+            <NavLink to="/books" className={styles.fontLink}>
+              "Books"
+            </NavLink>
+          </p>
+        </div>
+      ) : (
         <NavLink to="/books" className={styles.link}>
-          If you see this text it means that you are not authorized! <br />
-          If you want to see Books ----
-          <span className={styles.click}> just click here</span>
+          <span className={styles.click}> just click</span>
         </NavLink>
       )}
     </div>
