e354fb85149d6bc455a7ff258cc4c81b5624d580
RomaSamkov <Romansamkov.rs@gmail.com>
2023-02-27 21:36:58 +0200

    Update useForm.js

--- src/shared/hooks/useForm.js
+++ src/shared/hooks/useForm.js
@@ -1,4 +1,7 @@
 import { useState } from "react";
+import { toast } from "react-hot-toast";
+import { useSelector } from "react-redux";
+import { getAuthError } from "redux/auth/auth-selectors";
 
 const useForm = ({ initialState, onSubmit }) => {
   const [state, setState] = useState({ ...initialState });
@@ -12,11 +15,14 @@ const useForm = ({ initialState, onSubmit }) => {
       [name]: newValue,
     }));
   };
-
+  const { status } = useSelector(getAuthError);
   const handleSubmit = (e) => {
     e.preventDefault();
     onSubmit({ ...state });
     setState({ ...initialState });
+    if (status) {
+      toast.error("Not correct email or email already exist !");
+    }
   };
 
   return { state, setState, handleChange, handleSubmit };
