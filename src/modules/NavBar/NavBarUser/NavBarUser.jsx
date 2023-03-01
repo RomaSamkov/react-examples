3623adf1b16dd9a019c2ace475d3067783583d13
RomaSamkov <Romansamkov.rs@gmail.com>
2023-03-01 21:12:48 +0200

    Update NavBarUser.jsx

--- src/modules/NavBar/NavBarUser/NavBarUser.jsx
+++ src/modules/NavBar/NavBarUser/NavBarUser.jsx
@@ -1,6 +1,7 @@
 import { useDispatch, useSelector } from "react-redux";
 import { logout } from "redux/auth/auth-operations";
 import { getUser } from "redux/auth/auth-selectors";
+import styles from "./NavBarUser.module.scss";
 
 const NavBarUser = () => {
   const { name } = useSelector(getUser);
@@ -10,8 +11,9 @@ const NavBarUser = () => {
 
   return (
     <div>
-      <span>{name}</span> |{" "}
-      <button type="button" onClick={onLogout}>
+      <span className={styles.name}>{name}</span>
+      <span className={styles.span}>✔</span> |{" "}
+      <button className={styles.btn} type="button" onClick={onLogout}>
         Log Out
       </button>
     </div>
